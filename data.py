c120a40f714aeddc99eb9a7dc93aac5bef644671
Cesar Erives <cesarerives43@gmail.com>
2021-08-11 22:14:41 -0600

    Changed system to only update

--- data.py
+++ data.py
@@ -19,39 +19,32 @@ def newPatient(name):
     print (r)
 
 
-def updatePatient(id,name):
+def updatePatient(id):
     oxy = random.randint(90,100) #Oxygen level
     bp = random.randint(60,120) #Heartbeat  
     sug = random.randint(80,140)#Sugar 
     link= 'https://iot--api.herokuapp.com/_id:'
     link= link + id
-    r = requests.put(link, json ={'name':name , 'blood_pressure':bp , 'sugar_level':sug, 'oxygen_level':oxy})
+    r = requests.put(link, json ={'blood_pressure':bp , 'sugar_level':sug, 'oxygen_level':oxy})
     print("Response from Server: ")
     print (r)
 
 
 print("HostVital Data Sim")
-print("Type 1 to send new patient data or 2 to send data to an existing patient: ")
-menuoption = int(input())
-if menuoption == 1: 
-    print("Enter Username: ")
-    name= input()
-    newPatient(name)
-elif menuoption == 2:
-    print("Enter ID: ")
-    id=input()
-    print("Enter Name: ")
-    name=input()
-    print("Do you want to update the data continuously? Type 1 for yes and 2 for no: ")
-    decision = int(input())
-    if decision==1:
-        i=False
-        print("Use CTRL+C to stop.")
-        while i==False:
-            updatePatient(id,name)
-            time.sleep(5)
-    else:
-        updatePatient(id,name)
+print("At this time we can only update added patients. Doctors must sign new patients up.")
+print("Enter ID: ")
+id=input()
+
+print("Do you want to update the data continuously? Type 1 for yes and 2 for no: ")
+decision = int(input())
+if decision==1:
+    i=False
+    print("Use CTRL+C to stop.")
+    while i==False:
+        updatePatient(id)
+        time.sleep(5)
+else:
+    updatePatient(id)
 
 # send_data()
 
