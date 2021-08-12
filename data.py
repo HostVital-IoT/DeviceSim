a9eb99d60fc7f5f2133fdf3474adf7c7329d757c
Cesar Erives <cesarerives43@gmail.com>
2021-08-11 22:17:22 -0600

    Changed system to simulate the device

--- data.py
+++ data.py
@@ -8,16 +8,6 @@ import time
 bp = 0
 oxy = 0
 sug = 0
-name= ''
-
-def newPatient(name):
-    oxy = random.randint(90,100) #Oxygen level
-    bp = random.randint(60,120) #Heartbeat  
-    sug = random.randint(80,140)#Sugar  
-    r = requests.post('https://iot--api.herokuapp.com/new/', json ={'name':name , 'blood_pressure':bp , 'sugar_level':sug, 'oxygen_level':oxy})
-    print("Response from Server: ")
-    print (r)
-
 
 def updatePatient(id):
     oxy = random.randint(90,100) #Oxygen level
@@ -34,7 +24,6 @@ print("HostVital Data Sim")
 print("At this time we can only update added patients. Doctors must sign new patients up.")
 print("Enter ID: ")
 id=input()
-
 print("Do you want to update the data continuously? Type 1 for yes and 2 for no: ")
 decision = int(input())
 if decision==1:
